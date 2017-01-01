f11f8610cca952512df57a43f73ba241b6de2139
Guilherme Lima <guilhermelimak@gmail.com>
2017-01-01 18:32:14 -0200

    Add initial state

---
+++ src/state.js
@@ -0,0 +1,19 @@
+export default {
+  cards: {
+    list: [],
+    currentSelected: null,
+  },
+  cardsList: {
+    list: [],
+  },
+  projects: {
+    list: [],
+    currentSelected: null,
+  },
+  UI: {
+    isSidebarOpen: false,
+    isImportModalOpen: false,
+    isWelcomeScreenOpen: false,
+    isSpinnerOpen: false,
+  },
+}

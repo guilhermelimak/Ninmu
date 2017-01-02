84792b75fb4ce1d1935cad315adb58866d977d01
Guilherme Lima <guilhermelimak@gmail.com>
2017-01-02 00:25:43 -0200

    Add logo placeholder

---
+++ src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="logo__wrapper">
+  <h1 class="logo__text">Ninmu</h1>
+  <h3 class="logo__text">任務</h3>
+</div>
+</template>
+
+<style lang="sass">
+.logo
+  &__wrapper
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+
+  &__text
+    display: block;
+    font-family: 'Menlo', sans-serif;
+</style>

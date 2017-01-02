ce21fc96ddeae173b70a7eba1b1c803cb36756e9
Guilherme Lima <guilhermelimak@gmail.com>
2017-01-02 00:28:52 -0200

    Add html and App component css

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,17 @@ export default {
 
 <style lang="sass">
 @import 'src/sass/app';
+
+#app,
+html,
+body
+  font-family: 'Helvetica', sans-serif;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  position: fixed;
+
 </style>

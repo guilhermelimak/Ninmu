6c204e73afad2d8ef626005044aeeaba30ffc1c4
Guilherme Lima <guilhermelimak@gmail.com>
2017-01-02 01:18:32 -0200

    Remove old actions

--- src/actions/uiActions.js
+++
@@ -1,3 +0,0 @@
-export const toggleImportModal = ({ dispatch }) => dispatch('TOGGLE_IMPORT_MODAL')
-export const toggleSidebar = ({ dispatch }) => dispatch('TOGGLE_SIDEBAR')
-export const hideWelcomeScreen = ({ dispatch }) => dispatch('HIDE_WELCOME_SCREEN')

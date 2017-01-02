706bd6785df658fcaf3d7eb31fc024f23173c823
Guilherme Lima <guilhermelimak@gmail.com>
2017-01-02 01:19:23 -0200

    Add WelcomeScreen component

---
+++ src/components/WelcomeScreen.vue
@@ -0,0 +1,21 @@
+<template>
+<div></div>
+</template>
+
+<script>
+import { auth } from 'src/store'
+
+export default {
+  created() {
+    if (auth.currentUser) {
+      this.$router.push('/choose-project')
+    } else {
+      this.$router.push('/login')
+    }
+  },
+}
+</script>
+
+<style lang="sass">
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import LoginLayout from 'components/LoginLayout'
+import WelcomeScreen from 'components/WelcomeScreen'
 import ChooseProjectLayout from 'components/ChooseProjectLayout'
 
 export default [
-  { path: '/', component: LoginLayout },
+  { path: '/', component: WelcomeScreen },
+  { path: '/login', component: LoginLayout },
   { path: '/choose-project', component: ChooseProjectLayout },
 ]

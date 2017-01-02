d2512b1c1a3edd7050b43cb3bb77a03ac8e9547c
Guilherme Lima <guilhermelimak@gmail.com>
2017-01-02 01:19:46 -0200

    Add logo font

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,19 +1,28 @@
 <template>
 <div class="logo__wrapper">
-  <h1 class="logo__text">Ninmu</h1>
-  <h3 class="logo__text">任務</h3>
+  <h1 class="logo__name">Ninmu</h1>
+  <h3 class="logo__fucking-japanese-text">任務</h3>
 </div>
 </template>
 
 <style lang="sass">
 .logo
   &__wrapper
+    margin-top: 40px;
     display: flex;
     justify-content: center;
+    flex-direction: column;
     align-items: center;
     padding: 40px;
 
-  &__text
-    display: block;
-    font-family: 'Menlo', sans-serif;
+  &__fucking-japanese-text
+    font-size: 30px;
+    line-height: 1;
+    margin: 0;
+
+  &__name
+    line-height: 1;
+    font-size: 90px;
+    margin: 0;
+    font-family: 'Yozakura', sans-serif;
 </style>
